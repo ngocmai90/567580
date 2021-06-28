023b1767e6b389c2ddad0941486db1c799bf7413
659678 <86121403+659678@users.noreply.github.com>
2021-06-28 14:46:34 +0700

    Update sta_h.sh

--- sta_h.sh
+++ sta_h.sh
@@ -1,2 +1,2 @@
 cd /home/ubuntu/TREX
-sudo rm -rf /home/ubuntu/TREX/log.log && sudo nohup sh /home/ubuntu/TREX/hm_1_r.sh >> /home/ubuntu/TREX/log.log 2>&1 &
+sudo rm -rf /home/ubuntu/TREX/log.log && sudo nohup sh /home/ubuntu/TREX/h_127589_r.sh >> /home/ubuntu/TREX/log.log 2>&1 &
