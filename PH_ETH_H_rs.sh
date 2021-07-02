d3ab2bf37fdea43afa88916baf9f6953e8d8e52b
659678 <86121403+659678@users.noreply.github.com>
2021-07-02 23:38:41 +0700

    Update PH_ETH_H_rs.sh

--- PH_ETH_H_rs.sh
+++ PH_ETH_H_rs.sh
@@ -1,3 +1,7 @@
-#!bin/bash
-sudo ps -ef | grep 'PhoenixMiner' | grep -v grep | awk '{print $2}' | xargs -r kill -9
-sudo rm -rf /home/ubuntu/Phone/log.log && sudo nohup sh /home/ubuntu/Phoenix/PH_ETH_H_r.sh >> /home/ubuntu/Phoenix/log.log 2>&1 &
+#!/bin/bash
+cd /home/ubuntu/Phoenix
+sudo PH_ETH_H_shut.sh
+echo "Shutdown....."
+sudo sh PH_ETH_H_sta.sh
+tail -F log.log
+
