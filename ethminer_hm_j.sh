5d7056ccc2891af93d904a15f97d747e3a1a6eb7
659678 <86121403+659678@users.noreply.github.com>
2021-07-03 00:40:30 +0700

    Update ethminer_hm_j.sh

--- ethminer_hm_j.sh
+++ ethminer_hm_j.sh
@@ -6,8 +6,8 @@ then
           echo "ethminer_running"
             exit 0
     else
-              echo "PhoenixMiner_stop"
-                                sudo rm -rf /home/ubuntu/bin/log.log && sudo nohup sh /home/ubuntu/bin/ethminer_hm_r.sh >> /home/ubuntu/bin/log.log 2>&1 &
+              echo "ethminer_stop"
+                                sudo rm -rf /home/ubuntu/log.log && sudo nohup sh /home/ubuntu/bin/ethminer_hm_r.sh >> /home/ubuntu/log.log 2>&1 &
                                   echo "ethminer_started"
 
                           fi
