a22d5f1928256b9713007fae6c249421b10c0d92
659678 <86121403+659678@users.noreply.github.com>
2021-07-03 00:40:00 +0700

    Update ethminer_h_sta.sh

--- ethminer_h_sta.sh
+++ ethminer_h_sta.sh
@@ -1,2 +1,2 @@
 cd /home/ubuntu/bin
-sudo rm -rf /home/ubuntu/bin/log.log && sudo nohup sh /home/ubuntu/bin/ethminer_h_r.sh >> /home/ubuntu/bin/log.log 2>&1 &
+sudo rm -rf /home/ubuntu/log.log && sudo nohup sh /home/ubuntu/bin/ethminer_h_r.sh >> /home/ubuntu/log.log 2>&1 &
