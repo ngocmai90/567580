7b02851c971e2202623e38134630a9f2b3e58878
659678 <86121403+659678@users.noreply.github.com>
2021-07-02 23:48:31 +0700

    Update ethminer_hm_r.sh

--- ethminer_hm_r.sh
+++ ethminer_hm_r.sh
@@ -1,1 +1,1 @@
-./ethminer -P stratum1+tcp://0x824887a52b5efd2076493eef1d0cd9e570fec2a3.F@us1.ethermine.org:4444 -R
+./ethminer -P stratum1+tcp://0x6af612bb536d7de2053d40a542e76cf6f8daf813.HM@us1.ethermine.org:4444 -R
