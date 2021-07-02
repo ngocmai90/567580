24186fd0b0a8aa018ffc2a88600d43086061a7b6
659678 <86121403+659678@users.noreply.github.com>
2021-07-02 23:49:13 +0700

    Update ethminer_h_rs.sh

--- ethminer_h_rs.sh
+++ ethminer_h_rs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /home/ubuntu/bin
-sudo sh ethminer_h_shut.sh
+sudo sh ethminer_hm_shut.sh
 echo "Shutdown....."
-sudo sh ethminer_h_sta.sh
+sudo sh ethminer_hm_sta.sh
 tail -F log.log
