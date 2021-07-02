7483b6ff4cd183b50651d136fa23ab73be5d8c8d
659678 <86121403+659678@users.noreply.github.com>
2021-07-03 00:49:03 +0700

    Update h_376286_rs.sh

--- h_376286_rs.sh
+++ h_376286_rs.sh
@@ -1,3 +1,3 @@
 #!bin/bash
 sudo ps -ef | grep 't-rex' | grep -v grep | awk '{print $2}' | xargs -r kill -9
-sudo rm -rf /home/ubuntu/TREX/log.log && sudo nohup sh /home/ubuntu/TREX/h_127589_r.sh >> /home/ubuntu/TREX/log.log 2>&1 &
+sudo rm -rf /home/ubuntu/log.log && sudo nohup sh /home/ubuntu/TREX/h_127589_r.sh >> /home/ubuntu/log.log 2>&1 &
