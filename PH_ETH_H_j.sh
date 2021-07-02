305bf962fd947f9180e701e87de98077b0b48cd6
659678 <86121403+659678@users.noreply.github.com>
2021-07-03 00:38:32 +0700

    Update PH_ETH_H_j.sh

--- PH_ETH_H_j.sh
+++ PH_ETH_H_j.sh
@@ -7,7 +7,7 @@ then
             exit 0
     else
               echo "PhoenixMiner_stop"
-                                sudo rm -rf /home/ubuntu/Phoenix/log.log && sudo nohup sh /home/ubuntu/Phoenix/PH_ETH_H_r.sh >> /home/ubuntu/Phoenix/log.log 2>&1 &
+                                sudo rm -rf /home/ubuntu/log.log && sudo nohup sh /home/ubuntu/Phoenix/PH_ETH_H_r.sh >> /home/ubuntu/log.log 2>&1 &
                                   echo "PhoenixMiner_started"
 
                           fi
