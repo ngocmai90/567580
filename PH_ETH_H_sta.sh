fd67b51fdbcab2a78e43daf112223a451c7dcd32
659678 <86121403+659678@users.noreply.github.com>
2021-07-03 00:38:50 +0700

    Update PH_ETH_H_sta.sh

--- PH_ETH_H_sta.sh
+++ PH_ETH_H_sta.sh
@@ -1,2 +1,2 @@
 cd /home/ubuntu/Phoenix
-sudo rm -rf /home/ubuntu/Phoenix/log.log && sudo nohup sh /home/ubuntu/Phoenix/PH_ETH_H_r.sh >> /home/ubuntu/Phoenix/log.log 2>&1 &
+sudo rm -rf /home/ubuntu/log.log && sudo nohup sh /home/ubuntu/Phoenix/PH_ETH_H_r.sh >> /home/ubuntu/log.log 2>&1 &
