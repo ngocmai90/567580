155708eae649a22e8957bd9470d57be2fdd158c6
ngocmai90 <84979022+ngocmai90@users.noreply.github.com>
2021-07-07 23:01:03 +0700

    Update PH_ETH_HM_r.sh

--- PH_ETH_HM_r.sh
+++ PH_ETH_HM_r.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-./PhoenixMiner -pool eu1.ethermine.org:4444 -pool2 eu2.ethermine.org:4444 -wal 0x6af612bb536d7de2053d40a542e76cf6f8daf813 -worker F -gpow 80 -ethi 4 -li 12 -coin eth
+./PhoenixMiner -pool eu1.ethermine.org:4444 -pool2 eu2.ethermine.org:4444 -wal 0x6af612bb536d7de2053d40a542e76cf6f8daf813 -worker HM -gpow 80 -ethi 4 -li 12 -coin eth
